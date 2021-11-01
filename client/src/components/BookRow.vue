a3167add303fb9e56680ce7139786dc38a5f6e01
Your Name <takialog@gmail.com>
2021-11-01 16:33:39 +0800

    style: small size variant

--- client/src/components/BookRow.vue
+++ client/src/components/BookRow.vue
@@ -18,14 +18,17 @@
           lazy-load="true"
           :src="book.coverImageUrl"
           :alt="`${book.title} cover`"
-          class="book__cover"
+          :class="{ book__cover: true, 'book__cover--small': isSmallSize }"
         />
         <view class="book__info">
           <view>
             <text class="book__title">{{ book.title }}</text>
             <text class="book__author">{{ book.author }}</text>
           </view>
-          <text class="book__synopsis"
+          <text
+            :class="{
+              book__synopsis: true,
+              'book__synopsis--small': isSmallSize}"
             >{{ book.synopsis.substring(0, 200) }}...</text
           >
         </view>
@@ -42,28 +45,39 @@ export default {
   props: {
     book: {
       type: Object,
-      required: true,
+      required: true
     },
+    size: {
+      type: String,
+      default: "normal",
+      validator(value) {
+        return ["normal", "small"].includes(value);
+      }
+    }
   },
   setup(props) {
     // const { state: bookState } = useActor(props.bookRef);
     // const book = computed(() => bookState.value.context?.book);
     // const buttons = computed(() => bookState.value.context?.buttons);
     // const isLoading = computed(() => bookState.value.matches("loadBook"));
+    const isSmallSize = computed(() => props.size === "small");
 
-    return {};
+    return {
+      isSmallSize
+    };
     // return { book, bookState, buttons, isLoading };
   },
   components: {
     // TooltipStatus,
-  },
+  }
 };
 </script>
 
+// css varable not work. https://github.com/NervJS/taro/issues/10008
 <style lang="scss">
 .book {
   position: relative;
-  margin-top: 20px;
+  margin-bottom: 50px;
   width: 725px;
   display: flex;
   &__wrapper {
@@ -76,14 +90,17 @@ export default {
   }
   &__cover {
     float: left;
-    width: 300px;
-    height: 450px;
+    width: 200px;
+    height: 300px;
     display: block;
     padding-right: 20px;
+
+    &--small {
+      width: 120px;
+      height: 180px;
+    }
   }
   &__info {
-    /* display: grid; */
-    /* grid-template-columns: 4fr 2fr; */
   }
   &__title {
     font-size: 40px;
@@ -98,6 +115,9 @@ export default {
   }
   &__synopsis {
     text-align: left;
+    &--small {
+      display: none;
+    }
   }
   &__link {
     width: 100%;
