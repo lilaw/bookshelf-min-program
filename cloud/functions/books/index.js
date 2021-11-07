b48eed38b54e14ef629c1e1afbac4eab8413a83d
Your Name <takialog@gmail.com>
2021-11-07 16:08:13 +0800

    feat: filter our book in listItem

--- cloud/functions/books/index.js
+++ cloud/functions/books/index.js
@@ -3,10 +3,19 @@ cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV,
 });
 const db = cloud.database();
-const dbCommand = db.command;
+const $ = db.command.aggregate;
 
 exports.main = async (event, context) => {
-  var books;
+  // find book by Id
+  if (event.bookId) {
+    const bookResult = await db.collection("books").doc(event.bookId).get();
+
+    return {
+      event,
+      book: bookResult.data,
+    };
+  }
+
   // Search book
   if (event.bookName) {
     const searchResult = await db
@@ -19,15 +28,41 @@ exports.main = async (event, context) => {
         }),
       })
       .get();
-    books = searchResult.data;
+    var books = searchResult.data;
   } else {
     // return random recommended books
-    const recommendedBooks = await db.collection("books").limit(10).get();
-    books = recommendedBooks.data;
+    const wxContext = cloud.getWXContext();
+    let bookInListItemPromise = db
+      .collection("users")
+      .aggregate()
+      .match({
+        openId: wxContext.OPENID,
+      })
+      .lookup({
+        from: "listItems",
+        localField: "_id",
+        foreignField: "ownerId",
+        as: "bookList",
+      })
+      .end()
+      .then((res) => res.list[0].bookList.map((item) => item.bookId));
+
+    const recommendedBooksPromise = db
+      .collection("books")
+      .skip(event.pageNum * 10)
+      .limit(10)
+      .get();
+      
+    const bookInListItem = await bookInListItemPromise
+    const recommendedBooks = await recommendedBooksPromise
+    var books = recommendedBooks.data.filter(
+      (item) => !bookInListItem.includes(item._id)
+    );
   }
 
   return {
     event,
     books,
+    dataInPage: event.pageNum + 1,
   };
 };
